30ecdd2ce3f8c25cfbecb9fb8d6038a822750557
Joshua Obinna <joshuaobinna93@gmail.com>
2019-09-04 23:38:53 +0100

    [fix]error

--- validate-form.js
+++ validate-form.js
@@ -21,3 +21,13 @@ function validateForm() {
         return true;
     }
 }
+
+// function submit_by_id() {
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     if (validation()) // Calling validation function
+//     {
+//     document.getElementById("form_id").submit(); //form submission
+//     alert(" Name : " + name + " n Email : " + email + " n Form Id : " + document.getElementById("form_id").getAttribute("id") + "nn Form Submitted Successfully......");
+//     }
+//     }
